7b14fa76caddb92379a47195832cf939e0b9db9a
bhanu723 <31636779+bhanu723@users.noreply.github.com>
2017-12-03 21:09:04 -0500

    Update GitTestProg.java

--- GitTestProg.java
+++ GitTestProg.java
@@ -5,8 +5,6 @@ public static void main(String[] args)
 {
 System.out.print("Hello Git....");
 System.out.println("my 2nd line of code");
-String St = "Bhanu"
-System.out.println(" i = "+i);
 System.out.println("Surekha welcome to Programming - github");
 System,out.println("FUnction_Surekha Branch")
 }
